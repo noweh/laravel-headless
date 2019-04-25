06c7e71c35d9e05723fe3b7e5f2fa2faab30270e
j.schmitt <j.schmitt@mazarinedigital.com>
2019-04-25 18:16:58 +0200

    prepare questionnaire controller

--- website/app/Exceptions/Handler.php
+++ website/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -29,8 +30,9 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -46,6 +48,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
--- website/app/Http/Controllers/QuestionnaireController.php
+++ website/app/Http/Controllers/QuestionnaireController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuestionnaireResource;
 use App\Models\Questionnaire;
+use App\Models\Theme;
 use Illuminate\Http\Request;
 
 class QuestionnaireController extends Controller
@@ -10,76 +12,89 @@ class QuestionnaireController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return QuestionnaireResource::collection(
+            Questionnaire::with([
+                //'themes',
+                //'questions',
+                //'questions.possibleAnswers',
+                //'questions.goodAnswer'
+            ])->paginate(25)
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return QuestionnaireResource
      */
     public function store(Request $request)
     {
-        //
+        $theme = Theme::where('code', $request->theme_code)->firstOrFail();
+
+        /*if (!$theme) {
+            return response()->json([
+                'error' => 'Theme not found'
+            ], 404);
+        }*/
+
+        $questionnaire = Questionnaire::create([
+            'published' => $request->published,
+            'level' => $request->level,
+            'note_max' => $request->note_max,
+            'active:fr' => $request->{'active:fr'},
+            'title:fr' => $request->{'title:fr'},
+            'description:fr' => $request->{'description:fr'},
+            'active:en' => $request->{'active:en'},
+            'title:en' => $request->{'title:en'},
+            'description:en' => $request->{'description:en'},
+        ]);
+
+        $questionnaire->themes()->attach($theme->toArray());
+
+        return new QuestionnaireResource($questionnaire);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  Questionnaire  $questionnaire
-     * @return \Illuminate\Http\Response
+     * @return QuestionnaireResource
      */
     public function show(Questionnaire $questionnaire)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  Questionnaire  $questionnaire
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Questionnaire $questionnaire)
-    {
-        //
+        return new QuestionnaireResource($questionnaire);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  Questionnaire  $questionnaire
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @param  Questionnaire $questionnaire
+     * @return QuestionnaireResource
      */
     public function update(Request $request, Questionnaire $questionnaire)
     {
-        //
+        $questionnaire->update($request);
+
+        return new QuestionnaireResource($questionnaire);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  Questionnaire  $questionnaire
+     * @param  Questionnaire $questionnaire
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function destroy(Questionnaire $questionnaire)
     {
-        //
+        $questionnaire->delete();
+
+        return response()->json(null, 204);
     }
 }
